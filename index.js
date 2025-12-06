9461ad9b739ec47d71905bf01616a88c6862b615
sorkar5sourav <sorkar.s.sourav@gmail.com>
2025-12-06 22:36:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+const port = process.env.PORT || 3000;
+// const crypto = require("crypto");
+// middleware
+app.use(express.json());
+app.use(cors());
+// const admin = require("firebase-admin");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jskgf2c.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running just fine!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
